09cec8b802c3910413eff0d1a00e951eee9259e2
Brook Miller <millerbrook@gmail.com>
2021-08-09 12:35:04 -0400

    Refactors keypress functions and adds new quit feature

--- ship.py
+++ ship.py
@@ -21,11 +21,12 @@ class Ship:
 
         # Store a decimal value for the ship's horizontal position
         self.x = float(self.rect.x)
+        
     def update(self):
         """Update the ship's value, not the rect"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         
         #Update rect object from self.x
