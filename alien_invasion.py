08b903195afc4f75d44ee63009bd5d7bfec4d2b9
Brook Miller <millerbrook@gmail.com>
2021-08-11 10:17:22 -0400

    Works on fleet motion; error still in alien.py?

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,9 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
+
 
     # Watch for keyboard and mouse events
     def _check_events(self):
@@ -75,6 +77,11 @@ class AlienInvasion:
         """Update position of bullets and get rid of old bullets."""
         #Update bullet positions
         self.bullets.update()
+    
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update the position of all aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
@@ -108,6 +115,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2*alien.rect.height *row_number
         self.aliens.add(alien)
+    
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         # Redraw screen during each pass through the loop
